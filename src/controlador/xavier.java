6ba71f3d3147a113049317fe8192365998a2d574
Invitado_PC05 <Invitado_PC05@LAB06_PC05>
2017-10-20 12:39:11 -0500

    kajfklajfklahjfhjkashriowebtrvnhtrio5thvt

---
+++ src/controlador/xavier.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controlador;
+
+/**
+ *
+ * @author Invitado_PC05
+ */
+public class xavier {
+    //7hola amiguitos jaja 
+            
+}
