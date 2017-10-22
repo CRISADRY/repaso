85190ee5269d53998a5c70d45275b3e8c708e442
pc <pc@KrisMa>
2017-10-22 16:53:20 -0500

    Agrego clase mensaje en el proyecto controlador

---
+++ src/controlador/mensaje.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controlador;
+
+
+public class mensaje {
+    
+    public void primero(){
+        System.out.println("Sigo repasando");
+    }
+    
+}
