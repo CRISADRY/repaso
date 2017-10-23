d0a212a6172b75edd639392fc445e09054625375
Diego Almache <>
2017-10-23 10:23:22 -0500

    Almache Diego

---
+++ src/controlador/AlmacheDiego.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controlador;
+
+/**
+ *
+ * @author Invitado_PC16
+ */
+public class AlmacheDiego {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Hola
+    }
+}
